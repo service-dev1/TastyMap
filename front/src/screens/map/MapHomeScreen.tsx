5c99cbed61bac09e6b74ef5da777144fb49b0490
[개발팀] 박종환 <whdghks94@gmail.com>
2024-09-24 17:04:35 +0900

    스터디 자료 업로드

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,30 +1,111 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useRef} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import useAuth from '@/hooks/queries/useAuth';
+import {colors} from '@/constants';
+import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
+import useUserLocation from '@/hooks/useUserLocation';
+import usePermission from '@/hooks/usePermission';
+import mapStyle from '@/style/mapStyle';
+
+type Navigation = CompositeNavigationProp<
+  StackNavigationProp<MapStackParamList>,
+  DrawerNavigationProp<MainDrawerParamList>
+>;
 
 function MapHomeScreen() {
+  const inset = useSafeAreaInsets();
   const {logoutMutation} = useAuth();
+  const navigation = useNavigation<Navigation>();
+  const mapRef = useRef<MapView | null>(null);
+  const {userLocation, isUserLocationError} = useUserLocation();
+  usePermission('LOCATION');
 
   const handleLogout = () => {
     logoutMutation.mutate(null);
   };
 
+  const handlePressUserLocation = () => {
+    if (isUserLocationError) {
+      // 에러메세지를 표시하기
+      return;
+    }
+
+    mapRef.current?.animateToRegion({
+      latitude: userLocation.latitude,
+      longitude: userLocation.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  };
+
   return (
-    <MapView
-      style={styles.container}
-      provider={PROVIDER_GOOGLE}
-      showsUserLocation
-      followsUserLocation
-      showsMyLocationButton={true}
-    />
+    <>
+      <MapView
+        ref={mapRef}
+        style={styles.container}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        followsUserLocation
+        showsMyLocationButton={false}
+        customMapStyle={mapStyle}
+      />
+      <Pressable
+        style={[styles.drawerButton, {top: inset.top || 20}]}
+        onPress={() => navigation.openDrawer()}>
+        <Ionicons name="menu" color={colors.WHITE} size={25} />
+      </Pressable>
+      <View style={styles.buttonList}>
+        <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+          <MaterialIcons name="my-location" color={colors.WHITE} size={25} />
+        </Pressable>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  drawerButton: {
+    position: 'absolute',
+    left: 0,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    backgroundColor: colors.PINK_700,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.5,
+    elevation: 4,
+  },
+  buttonList: {
+    position: 'absolute',
+    bottom: 30,
+    right: 15,
+  },
+  mapButton: {
+    backgroundColor: colors.PINK_700,
+    marginVertical: 5,
+    height: 48,
+    width: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.5,
+    elevation: 2,
   },
 });
 
